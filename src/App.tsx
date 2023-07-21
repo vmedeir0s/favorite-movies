f2d8288cff5134dd5d96d94e76dc88ff1525d471
vmedeir0s <vmeedeiros@gmail.com>
2023-07-21 11:19:41 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Template Project
+      </h1>
+      <p>React + TS + Tailwind</p>
+    </>
+  )
+}
+
+export default App
