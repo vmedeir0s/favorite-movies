51bde4c53eecd48fda6aed6bd7b7d8913e016c81
vmedeir0s <vmeedeiros@gmail.com>
2023-10-16 10:48:13 -0300

    add data movies

---
+++ src/data.tsx
@@ -0,0 +1,59 @@
+import { MovieType } from './types';
+
+export const moviesData: MovieType[] = [
+  {
+    id: 1,
+    name: 'Lupin',
+    image:
+      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yUSpKDhibrxnY3EXAcNkaPrUOI0.jpg',
+    isFavorited: false,
+    description:
+      'Na adolescência, a vida de Assane Diop dá uma guinada radical quando seu pai morre após ser acusado de um crime que não cometeu. Vinte e cinco anos depois, Assane se inspira em "Arsène Lupin, o Ladrão de Casaca" para vingá-lo.',
+    releaseYear: 2021,
+    director: 'Marcela Said',
+  },
+  {
+    id: 2,
+    name: 'The Flash',
+    image:
+      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/vjEZrx9vSP40NKXhNxAGxvHxL9y.jpg',
+    isFavorited: false,
+    description:
+      'Os mundos colidem quando Barry Allen usa seus superpoderes para viajar no tempo e mudar os acontecimentos do passado. Mas a tentativa de salvar sua família altera inadvertidamente o futuro.',
+    releaseYear: 2023,
+    director: 'Andy Muschietti',
+  },
+  {
+    id: 3,
+    name: 'Loki',
+    image:
+      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/ukR0MkCQE6IylzBPd61txJi1L3E.jpg',
+    isFavorited: false,
+    description:
+      'Começando imediatamente depois que Loki rouba o Tesseract (de novo), ele se encontra diante da Autoridade de Variação Temporal, uma organização burocrática que existe fora do tempo e espaço. Forçado a responder por seus crimes contra a linha do tempo, ele recebe uma escolha: Ser deletado da realidade ou ajudar a capturar uma ameaça ainda maior.',
+    releaseYear: 2021,
+    director: 'Kate Herron',
+  },
+  {
+    id: 4,
+    name: 'Ahsoka',
+    image:
+      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/laCJxobHoPVaLQTKxc14Y2zV64J.jpg',
+    isFavorited: false,
+    description:
+      'Situada após a queda do Império, a série segue a ex-cavaleira Jedi Ahsoka Tano enquanto ela investiga uma ameaça emergente a uma galáxia vulnerável.',
+    releaseYear: 2023,
+    director: 'Dave Filoni',
+  },
+  {
+    id: 5,
+    name: 'Peaky Blinders',
+    image:
+      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/i0uajcHH9yogXMfDHpOXexIukG9.jpg',
+    isFavorited: false,
+    description:
+      'Thomas Shelby e seus irmãos retornam a Birmingham depois de servir no exército britânico durante a Primeira Guerra Mundial. Os Peaky Blinders, a gangue na qual Thomas é líder, controlam a cidade de Birmingham. Mas, como as ambições de Shelby se estendem para além de Birmingham, ele planeja construir o império de negócios que criou e impedir qualquer um que atrapalhar.',
+    releaseYear: 2013,
+    director: 'Steven Knight',
+  },
+];
